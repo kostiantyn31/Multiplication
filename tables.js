53895e794051f2db506b126f8bf2ff01aede1855
ChristopherSps <98600618+ChristopherSps@users.noreply.github.com>
2022-04-14 07:50:46 -0400

    Tables work, need to print out tables on page

--- tables.js
+++ tables.js
@@ -1,18 +1,5 @@
 /* This is where we print out the times tables */
 
-<<<<<<< Updated upstream
-var inter = document.createElement("inter");
-inner.setAttribute("id","inter");
-document.body.appendChild(inter);
-
-function tables(factor){
-    if(factor === "undefined"){
-        factor = prompt("Times tables for ");
-    }
-    let table = "Table for "+factor+ "\n";
-    for(let row = min; row <= max; row++){
-        table += factor+ " * " +row+ " = "+ factor*row + "\n";
-=======
 function tables(){
     let min = localStorage.getItem("min");
     let max = localStorage.getItem("max");
@@ -25,7 +12,6 @@ function tables(){
     for (let row = min; row <= max; row++) {
         tableText += factor+ " * " +row+ " = "+ factor*row + "\n";
         alert(tableText);
->>>>>>> Stashed changes
     }
-    alert(table);
+    alert(tableText);
 }
