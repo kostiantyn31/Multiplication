e0fd19d7a5b9e075955c635384aae0c12d8fd392
ChristopherSps <98600618+ChristopherSps@users.noreply.github.com>
2022-04-25 01:35:01 -0400

    created button for table

--- tables.js
+++ tables.js
@@ -1,17 +1,22 @@
 /* This is where we print out the times tables */
 
+var buttonTab = document.createElement("button");
+buttonTab.setAttribute("button","buttonTab");
+buttonTab.setAttribute("onClick","tables()");
+buttonTab.innertText="Tables";
+document.body.appendChild(buttonTab);
+
 function tables(){
     let min = localStorage.getItem("min");
     let max = localStorage.getItem("max");
     let factor = localStorage.getItem("problemFactor");
-    // if (factor === "undefined") {
-    //     alert("hey");
-    //     factor = prompt("Times tables for ");
-    // }
+    if (factor === "undefined") {
+        alert("hey");
+        factor = prompt("Times tables for ");
+    }
     let tableText = "Table for "+factor+ "\n";
     for (let row = min; row <= max; row++) {
         tableText += factor+ " * " +row+ " = "+ factor*row + "\n";
-        alert(tableText);
     }
     alert(tableText);
 }
