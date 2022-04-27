06720f418f93c0a6513dee2aea75efc596320232
ChristopherSps <98600618+ChristopherSps@users.noreply.github.com>
2022-04-27 11:51:01 -0400

    Fixed Everything Just Have To Do Input Box

--- tables.js
+++ tables.js
@@ -4,17 +4,18 @@ function tables(){
     let min = localStorage.getItem("min");
     let max = localStorage.getItem("max");
     let factor = localStorage.getItem("problemFactor");
-    var tbl = document.getElementById("candyTable");
-    var row = tbl.insertRow();
-    var cell1 = row.insertCell();
-    cell1.innerHTML = "hey";
-    if (factor === "undefined") {
-        alert("hey");
+    if (isNaN(factor)) {
         factor = prompt("Times tables for ");
     }
-    let tableText = "Table for "+factor+ "\n";
+    let tableText = "<tr><th>Table For Missed Candy, Factor <span>"+factor+"</span>!</th></tr>";
     for (let row = min; row <= max; row++) {
-        tableText += factor+ " * " +row+ " = "+ factor*row + "\n";
+        tableText += "<tr><td>"+factor+ " * " +row+ " = "+ factor*row + "</td></tr>";
     }
-    alert(tableText);
+   let timesTable = document.getElementById("candyTable");
+   timesTable.innerHTML = tableText;
 }
+
+// var tbl = document.getElementById("candyTable");
+// var row = tbl.insertRow();
+// var cell1 = row.insertCell();
+// cell1.innerHTML = "hey";
